4d2ce0707de5f367a768981bd328a4a59ca19064
kdy <kimdongkey23@gmail.com>
2025-11-07 13:23:03 +0900

    edit portfolio

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ document.querySelectorAll('a').forEach(link => {
     link.setAttribute('rel', 'noopener noreferrer');
 });
 
+<<<<<<< HEAD
 // ========== 스킬 섹션 아코디언 탭 ========== 
 const skillButtons = document.querySelectorAll('.skill-wrap');
 skillButtons.forEach(btn => {
@@ -41,3 +42,43 @@ $(document).ready(function(){
         arrows: true         
     });
 });
+=======
+// ========== 헤더 nav 이동 ========== 
+const navLinks = document.querySelectorAll('.nav-list a');
+navLinks.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href');
+        document.querySelector(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// ================= 헤더 스크롤 =================
+window.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY; 
+    const header = document.querySelector("header");
+
+    if (scrollTop >= 30) {
+        header.classList.add("fixed");   
+    } else {
+        header.classList.remove("fixed"); 
+    }
+});
+
+// ========== skill open btn ========== 
+const skillOpenBtn = document.querySelector('.open-btn');
+const skillDescList = document.querySelector('.skill-desc-list')
+skillOpenBtn.addEventListener('click',()=>{
+    skillOpenBtn.classList.toggle('close')
+    if(skillOpenBtn.classList.contains('close')){
+        skillDescList.style.maxHeight = '100%'
+        skillDescList.style.opacity = '1'
+        window.scrollBy({ top: 150, behavior: 'smooth' });
+    }else {
+        skillDescList.style.maxHeight = '0'
+        skillDescList.style.opacity = '0'
+    }
+})
+>>>>>>> c11cec2 (edit portfolio)

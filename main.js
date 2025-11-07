4ed85341a3c3c3af3911de311eb3a1fa7a8179ee
kdy <kimdongkey23@gmail.com>
2025-11-07 14:00:54 +0900

    edit portfolio

--- main.js
+++ main.js
@@ -4,45 +4,6 @@ document.querySelectorAll('a').forEach(link => {
     link.setAttribute('rel', 'noopener noreferrer');
 });
 
-<<<<<<< HEAD
-// ========== 스킬 섹션 아코디언 탭 ========== 
-const skillButtons = document.querySelectorAll('.skill-wrap');
-skillButtons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        const desc = btn.nextElementSibling;
-        skillButtons.forEach(otherBtn => {
-            const otherDesc = otherBtn.nextElementSibling;
-            if (otherBtn !== btn) {
-                otherBtn.classList.remove('active');
-                otherDesc.style.maxHeight = null;
-                otherDesc.style.opacity = 0;
-                otherDesc.style.padding = 0;
-            }
-        });
-        btn.classList.toggle('active');
-        if (btn.classList.contains('active')) {
-            desc.style.maxHeight = "200px";
-            desc.style.opacity = 1;
-            desc.style.padding = '1rem';
-        } else {
-            desc.style.maxHeight = null;
-            desc.style.opacity = 0;
-            desc.style.padding = 0;
-        }
-    });
-});
-
-// ========== 프로젝트 슬라이더 ==========
-$(document).ready(function(){
-    $('.project-slider').slick({
-        dots: true,          
-        infinite: false,     
-        slidesToShow: 1,    
-        slidesToScroll: 1,   
-        arrows: true         
-    });
-});
-=======
 // ========== 헤더 nav 이동 ========== 
 const navLinks = document.querySelectorAll('.nav-list a');
 navLinks.forEach(link => {
@@ -81,4 +42,3 @@ skillOpenBtn.addEventListener('click',()=>{
         skillDescList.style.opacity = '0'
     }
 })
->>>>>>> c11cec2 (edit portfolio)
